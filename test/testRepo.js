545c8c6b72849c1949bbacffdc55ddf8a573ace6
Jeff Philipp <jeffrey.philipp@gmail.com>
2017-04-24 12:03:00 -0400

    Test tests

---
+++ test/testRepo.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var rewire = require('rewire');
+
+var repo = rewire('../lib/repo.js');
+
+describe('lib/repo', function() {
+	describe('$makeCredentialsCallback()', function() {
+		let mcc = repo.__get__('makeCredentialsCallback');
+		it('should return a function', function() {
+			let res = mcc('', '');
+			assert.equal(typeof(res), 'function');
+		})
+	})
+})
